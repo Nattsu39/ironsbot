4c064d16edfaada1e59e6a0c5a8915a57419a8cd
Nattsu39 <nattsu39@outlook.com>
2026-03-31 12:14:22 +0800

    feat(get_seer_info): 更新GetImage以支持备用图片源并添加玩家头像/边框图片获取功能

--- ironsbot/plugins/get_seer_info/depends/image.py
+++ ironsbot/plugins/get_seer_info/depends/image.py
@@ -2,20 +2,36 @@ from nonebot.params import Depends
 
 from ironsbot.utils.image import GetImage
 
-PET_IMAGE_URL_TEMPLATE = "https://newseer.61.com/web/monster/body/{}.png"
-PetBodyImageGetter = GetImage(PET_IMAGE_URL_TEMPLATE)
+PetBodyImageGetter = GetImage(
+    "https://newseer.61.com/web/monster/body/{}.png",
+    "https://cnb.cool/SeerAPI/seer-unity-assets/-/git/raw/main/newseer/assets/art/ui/assets/pet/body/{}.png",
+)
 PetBodyImage = Depends(PetBodyImageGetter)
 
-
-MINTMARK_IMAGE_URL_TEMPLATE = "https://newseer.61.com/web/countermark/icon/{}.png"
-MintmarkBodyImageGetter = GetImage(MINTMARK_IMAGE_URL_TEMPLATE)
+MintmarkBodyImageGetter = GetImage(
+    "https://newseer.61.com/web/countermark/icon/{}.png",
+    "https://cnb.cool/SeerAPI/seer-unity-assets/-/git/raw/main/newseer/assets/art/ui/assets/countermark/icon/{}.png",
+)
 MintmarkBodyImage = Depends(MintmarkBodyImageGetter)
 
-
-ELEMENT_TYPE_IMAGE_URL_TEMPLATE = "https://newseer.61.com/web/PetType/{}.png"
-ElementTypeImageGetter = GetImage(ELEMENT_TYPE_IMAGE_URL_TEMPLATE)
+ElementTypeImageGetter = GetImage(
+    "https://newseer.61.com/web/PetType/{}.png",
+    "https://cnb.cool/SeerAPI/seer-unity-assets/-/git/raw/main/newseer/assets/art/ui/assets/pettype/{}.png",
+)
 ElementTypeImage = Depends(ElementTypeImageGetter)
 
-PREVIEW_IMAGE_URL_TEMPLATE = "https://cnb.cool/HurryWang/seer-unity-preview-img-dumper-cnb/-/git/raw/master/img/preview.png"
-PreviewImageGetter = GetImage(PREVIEW_IMAGE_URL_TEMPLATE)
+PreviewImageGetter = GetImage(
+    "https://cnb.cool/HurryWang/seer-unity-preview-img-dumper-cnb/-/git/raw/master/img/preview.png",
+)
 PreviewImage = Depends(PreviewImageGetter)
+
+
+AvatarHeadImageGetter = GetImage(
+    "https://cnb.cool/SeerAPI/seer-unity-assets/-/git/raw/main/newseer/assets/art/ui/assets/avatar/head/{}.png",
+)
+AvatarHeadImage = Depends(AvatarHeadImageGetter)
+
+AvatarFrameImageGetter = GetImage(
+    "https://cnb.cool/SeerAPI/seer-unity-assets/-/git/raw/main/newseer/assets/art/ui/assets/avatar/frame/{}.png",
+)
+AvatarFrameImage = Depends(AvatarFrameImageGetter)
