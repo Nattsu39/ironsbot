82ecf921f18d47a5ac0a8a24fe26ee205b13596f
Nattsu39 <nattsu39@outlook.com>
2026-05-18 15:52:29 +0800

    refactor(headless_seer): 优化错误提示信息

--- ironsbot/plugins/get_seer_info/depends/headless.py
+++ ironsbot/plugins/get_seer_info/depends/headless.py
@@ -6,6 +6,7 @@ require("ironsbot.plugins.headless_seer")
 
 from ironsbot.plugins.headless_seer.exception import (
     ClientNotInitializedError,
+    DisconnectedError,
     NotLoggedInError,
 )
 from ironsbot.plugins.headless_seer.game import SeerGame
@@ -16,9 +17,11 @@ async def _get_game_client(matcher: Matcher) -> SeerGame:
     try:
         return client_manager.get_client()
     except ClientNotInitializedError:
-        await matcher.finish("❌无头客户端未运行，无法使用此命令")
+        await matcher.finish("❌ 无头客户端尚未初始化，无法使用此命令")
     except NotLoggedInError:
-        await matcher.finish("❌ 无头客户端尚未登录，无法使用此命令！")
+        await matcher.finish("❌ 无头客户端尚未登录，无法使用此命令")
+    except DisconnectedError:
+        await matcher.finish("❌ 无头客户端连接已断开，正在尝试重连，请稍后再试")
 
 
 GameClient = Depends(_get_game_client)
